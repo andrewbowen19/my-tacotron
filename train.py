d3c3991e4ba23e7d19803d3a6b2e6df6b5059db0
Andrew Bowen <atbowen@gmail.com>
2020-12-01 21:53:40 -0500

    idek man

--- train.py
+++ train.py
@@ -75,7 +75,9 @@ def prepare_directories_and_logger(output_directory, log_directory, rank):
 
 
 def load_model(hparams):
+    print('Generating Tacotron2 model')
     model = Tacotron2(hparams)#.cuda()
+    print('Tacotron2 model generated!')
     if hparams.fp16_run:
         model.decoder.attention_layer.score_mask_value = finfo('float16').min
 
